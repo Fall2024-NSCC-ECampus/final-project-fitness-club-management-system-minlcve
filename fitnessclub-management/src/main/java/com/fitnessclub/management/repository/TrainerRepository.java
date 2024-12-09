3dbc76cfc8d5630df84655d53615f7958006f0f2
minlcve <146142173+minlcve@users.noreply.github.com>
2024-12-09 03:31:22 -0400

    Add files via upload

---
+++ fitnessclub-management/src/main/java/com/fitnessclub/management/repository/TrainerRepository.java
@@ -0,0 +1,10 @@
+package com.fitnessclub.management.repository;
+
+import com.fitnessclub.management.model.Trainer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrainerRepository extends JpaRepository<Trainer, Long> {
+
+}
---
+++ fitnessclub-management/src/test/java/com/fitnessclub/management/FitnessClubManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnessclub.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessClubManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
